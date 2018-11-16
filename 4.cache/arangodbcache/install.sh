cedef79307af163dfac626932121016d1bbe59db
jupiterfirepower@gmail.com <jupiterfirepower@gmail.com>
2018-11-16 13:10:32 +0200

    arango db

--- 4.cache/arangodbcache/install.sh
+++ 4.cache/arangodbcache/install.sh
@@ -8,10 +8,26 @@ sudo apt-get install arangodb3-dbg=3.3.19
 sudo service arangodb3 status
 arangosh
 
+db._create('docs');
+db._collections();
+
+db.docs.insert({_key: 'foo', value: 'bar'});
+db.docs.insert({_key: 'foo1', value: 'bar1'});
+db.docs.insert({_key: 'foo2', value: 'bar2'});
+db.docs.insert({_key: 'foo3', value: 'bar3'});
+db.docs.insert({_key: 'foo4', value: 'bar4'});
+
+db._createDatabase("testdb");
+db._useDatabase('testdb');
+db._databases();
+
+
 git clone https://github.com/arangodb/fuerte
 sudo apt-get install libssl-dev
 sudo apt-get install libboost-all-dev
 sudo apt-get install libcurl4-openssl-dev
 git clone https://github.com/arangodb/velocypack
+cd velocypack
 cmake --build .
+make install
 
